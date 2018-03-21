686b1dffc40ddf3505277779e585d10d2401868f
Lothandar <guillaume.letuppe96@gmail.com>
2018-03-21 15:43:50 +0000

    firstPush
    First Push

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GroupProject
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Quit_Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Login LoginWindow = new Login();
+            this.Hide();
+            LoginWindow.Show();
+        }
+    }
+}
